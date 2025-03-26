49eacc7425fd892c8c2e306b66c801d093847153
Jose Mauricio <Motrezt@gmail.com>
2025-03-26 13:05:02 -0600

    26 marzo

--- src/app/Components/NavBar.tsx
+++ src/app/Components/NavBar.tsx
@@ -7,7 +7,7 @@ export default function NavBar() {
 <nav className="w-64 bg-white p-8 border-r border-gray-300 text-black allign-contents justify-items">
   <ul className="flex flex-col gap-4">
       <li>
-     <Link href="/" className="flex items-center gap-4 hover:text-blue-600">
+     <Link href="/dashboard" className="flex items-center gap-4 hover:text-blue-600">
       <img 
        src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor'%3E%3Cpath stroke-linecap='round' stroke-linejoin='round' d='m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25'/%3E%3C/svg%3E"
        className="w-6 h-6"
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,47 @@
+export default function dashboard() {
+    return (
+      <div className="space-y-4">
+        {/* Primera fila */}
+        <div className="flex gap-4">
+          <div className="bg-gray-200 p-4 flex-1 rounded shadow text-black">
+            <h2 className="font-semibold">Contactos sugeridos</h2>
+            <p>[Contenido placeholder]</p>
+          </div>
+          <div className="bg-gray-200 p-4 flex-1 rounded shadow text-black">
+            <h2 className="font-semibold">Tareas pendientes</h2>
+            <p>[Contenido placeholder]</p>
+          </div>
+        </div>
+  
+        {/* Segunda fila (grid) */}
+        <div className="grid grid-cols-3 grid-rows-2 gap-4">
+          <div className="flex flex-col gap-4">
+          <div className="bg-gray-200 p-6 rounded shadow text-black">
+            <h2 className="font-semibold">Vistas a tu negocio</h2>
+            <p>[Contenido placeholder]</p>
+          </div>
+          <div className="bg-gray-200 p-6 rounded shadow text-black">
+            <h2 className="font-semibold">Nuevos productos</h2>
+            <p>[Contenido placeholder]</p>
+          </div>
+          </div>
+          
+          <div className="bg-gray-300 p-6 rounded-xl grid grid-cols-3 grid-rows-2 gap-4">
+          <div className="bg-gray-200 p-4 col-span-3 rounded shadow text-black">
+            <h2 className="font-semibold">Reporte del mes</h2>
+            <p>[Contenido placeholder]</p>
+          </div>
+          <div className="bg-gray-200 p-6 rounded shadow text-black">
+            <h2 className="font-semibold">Día actual</h2>
+            <p>[Contenido placeholder]</p>
+          </div>
+          <div className="bg-gray-200 p-6 rounded shadow text-black">
+            <h2 className="font-semibold">Crecimiento</h2>
+            <p>[Contenido placeholder]</p>
+          </div>
+        </div>
+      </div>
+      </div>
+    );
+  }
+  
